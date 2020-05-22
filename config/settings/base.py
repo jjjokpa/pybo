5b78c92bb6ce19f528810ac8913a8b6844ff1996
김동석 <jjjokpa13@gmail.com>
2020-05-22 15:39:22 +0900

    pybo 로거

--- config/settings/base.py
+++ config/settings/base.py
@@ -182,5 +182,9 @@ LOGGING = {
             'level': 'INFO',
             'propagate': False,
         },
+        'pybo': {
+            'handlers': ['console', 'file'],
+            'level': 'INFO',
+        },
     }
 }
